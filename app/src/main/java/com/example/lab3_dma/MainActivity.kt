020bbb2b6169a3d317db38cc0d392452a28e7716
demittheking <dkarasev42@gmail.com>
2023-04-07 02:16:25 +0600

    Added some new activities. Made a chat window with LinearLayout in Activity2

--- app/src/main/java/com/example/lab3_dma/MainActivity.kt
+++ app/src/main/java/com/example/lab3_dma/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.lab3_dma
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, Activity2::class.java)
+            startActivity(intent)
+        }
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            val intent = Intent(this, Activity3::class.java)
+            startActivity(intent)
+        }
+        val button3 = findViewById<Button>(R.id.button3)
+        button3.setOnClickListener {
+            val intent = Intent(this, Activity4::class.java)
+            startActivity(intent)
+        }
     }
+
 }
