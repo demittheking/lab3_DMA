8f46a7b7587f8c1a7d82ab0f8735da2721773ca2
demittheking <dkarasev42@gmail.com>
2023-04-07 02:38:48 +0600

    After all previous actions I placed an Image view inside the frame layout, and on top of the image button. I made a handler for the button. If it's pressed the pictures in the image view randomly change.Added arbitrary images to res/drawable.

--- app/src/main/java/com/example/lab3_dma/Activity4.kt
+++ app/src/main/java/com/example/lab3_dma/Activity4.kt
@@ -2,10 +2,29 @@ package com.example.lab3_dma
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.ImageView
+import kotlin.random.Random
 
 class Activity4 : AppCompatActivity() {
+    val imageArray: IntArray = intArrayOf(
+        R.drawable.image_1,
+        R.drawable.image_2,
+        R.drawable.image_3,
+    )
+    val random: Random = Random
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_4)
+        val layout: FrameLayout = findViewById(R.id.frameLayout)
+        val imageView: ImageView = findViewById(R.id.imageView)
+        val imageButton: ImageButton = findViewById(R.id.imageButton)
+        imageView.setImageResource(imageArray[0])
+        imageButton.setOnClickListener {
+            val randomNumber = random.nextInt(imageArray.size)
+            imageView.setImageResource(imageArray[randomNumber])
+        }
     }
+
 }
